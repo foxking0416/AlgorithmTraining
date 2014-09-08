44f7044a73d6d48b12a80c9ad15b5fa02c8733ac
foxking0416 <foxking0416@gmail.com>
2014-09-07 22:54:39 -0400

    short commit

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -6,29 +6,62 @@ public class Algorithm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		testSorting();
+
+	}
+	
+	
+	private static void testShortestPath(){
 		
+	}
+	
+	private static void testSorting(){
 	    int[] array = Sort.generateRandomArray();
 		for (int i : array) {
 			System.out.print(i);
 			System.out.print(", ");
 		}
-		
 		System.out.print("\n");
 		
-
-		int arrayFront[] = Arrays.copyOfRange(array, 0, 4);
-		int arrayBack[] = Arrays.copyOfRange(array, 5, 9);
 		
-//		Sort.selectionSort(array);
-//		Sort.insertionSort(array);
-//		Sort.mergeSort(array, 0, array.length-1);
-		Sort.quickSort(array, 0, array.length-1);
-		for (int i : array) {
+		int arrayToSort1[] = array.clone();
+		int arrayToSort2[] = array.clone();
+		int arrayToSort3[] = array.clone();
+		int arrayToSort4[] = array.clone();
+		
+		Sort.selectionSort(arrayToSort1);
+		System.out.print("Selection Sort Result: ");
+		for (int i : arrayToSort1) {
 			System.out.print(i);
 			System.out.print(", ");
 		}
+		System.out.print("\n");
+		
+		Sort.insertionSort(arrayToSort2);
+		System.out.print("Selection Sort Result: ");
+		for (int i : arrayToSort2) {
+			System.out.print(i);
+			System.out.print(", ");
+		}
+		System.out.print("\n");
+		
+		Sort.mergeSort(arrayToSort3, 0, arrayToSort3.length-1);
+		System.out.print("Selection Sort Result: ");
+		for (int i : arrayToSort3) {
+			System.out.print(i);
+			System.out.print(", ");
+		}
+		System.out.print("\n");
+		
+		
+		Sort.quickSort(arrayToSort4, 0, arrayToSort4.length-1);
+		System.out.print("Selection Sort Result: ");
+		for (int i : arrayToSort4) {
+			System.out.print(i);
+			System.out.print(", ");
+		}
+		System.out.print("\n");
+		
 	}
-	
-	
 
 }
